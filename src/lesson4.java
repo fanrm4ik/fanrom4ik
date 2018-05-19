d57812e5334ebfcee6424ec6f8a069786a69f8c5
FANGO <fanrom4ik@gmail.com>
2018-05-19 12:02:07 +0300

    lesson4

---
+++ src/lesson4.java
@@ -0,0 +1,25 @@
+public class lesson4 {
+    /* написать функцию расчета площади прямоугольника
+
+    функцию расчета x^3+x^2+1
+
+    функцию расчета sin войного угла
+     */
+
+    public static double perim (double a7, double b7) {
+        double pow = a7*b7;
+        return pow;
+        }
+
+        public static double func1 (double x){
+        double y = Math.pow(x,3) + Math.pow(x,2)+1;
+        return y;
+        }
+
+        public static double func2 (double pg){
+        double pg2 = Math.sin(2*Math.toRadians(pg));
+        return pg2;
+        }
+}
+
+
